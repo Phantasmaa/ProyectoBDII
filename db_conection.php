cb4d33c8ce88216aa624c529f37c83ed9b40ac48
S3bastianDiaz <103963357+S3bastianDiaz@users.noreply.github.com>
2023-07-13 00:36:26 -0500

    Update db_conection.php

--- db_conection.php
+++ db_conection.php
@@ -1,18 +1,10 @@
-$serverName = "nombre_del_servidor"; //Reemplazar con nombre del sevidor
-$connectionOptions = array(
-    "Database" => "BancoBPU", // Reemplaza con el nombre de la base de datos
-    "Uid" => "nombre_de_usuario", // Reemplaza con el nombre de usuario de SQL Server
-    "PWD" => "contraseña" // Reemplaza con la contraseña de SQL Server
-);
+$servername = 'nombre_del_servidor';
+$database = 'BancoBPU';
+$username = 'nombre_de_usuario';
+$password = 'contraseña';
 
-// Establecer la conexión
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-
-if ($conn === false) {
-    die(print_r(sqlsrv_errors(), true));
-}
-
-// Ejecutar consultas SQL y procesar los resultados aquí
-
-// Cerrar la conexión
-sqlsrv_close($conn);
+try {
+  $conn = new PDO("sqlsrv:Server=$servername;Database=$database", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  echo "Error de conexión: " . $e->getMessage();

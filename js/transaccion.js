c6bf2d8729d76db6dfd6ac6689dbf5de76eaec58
Phantasma <85528963+Phantasmaa@users.noreply.github.com>
2023-07-13 01:48:54 -0500

    Avance, diseño

--- js/transaccion.js
+++ js/transaccion.js
@@ -1,15 +1,24 @@
 function mostrarTarjeta(numTarjeta) {
     var tarjeta1 = document.getElementById("tarjeta1");
     var tarjeta2 = document.getElementById("tarjeta2");
+    var tarjeta3 = document.getElementById("tarjeta0");
     var opciones = document.getElementById("opciones");
+    var contenido = document.getElementById("procesoContainer");
 
     if (numTarjeta === 1) {
-        tarjeta1.style.display = "flex";
-        tarjeta2.style.display = "none";
+      tarjeta1.style.display = "flex";
+      tarjeta2.style.display = "none";
+      tarjeta3.style.display = "flex";
+      opciones.style.display = "none";
     } else if (numTarjeta === 2) {
-        tarjeta1.style.display = "none";
-        tarjeta2.style.display = "flex";
+      tarjeta1.style.display = "none";
+      tarjeta2.style.display = "flex";
+      tarjeta3.style.display = "flex";
+      opciones.style.display = "none";
+    } else if (numTarjeta === 0) {
+      tarjeta1.style.display = "none";
+      tarjeta2.style.display = "none";
+      tarjeta3.style.display = "none";
+      opciones.style.display = "flex";
     }
-
-    opciones.style.display = "none";
-}
+  }
